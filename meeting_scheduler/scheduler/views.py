3ad210b943b176c8c360558c80bc778d57c87d6c
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-12 13:20:42 +0200

    Views

--- meeting_scheduler/scheduler/views.py
+++ meeting_scheduler/scheduler/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Availability, Reservation
+from .serializers import AvailabilitySerializer, ReservationSerializer
+
 
 # Create your views here.
+
+class AvailabilityListCreateView(generics.ListCreateAPIView):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+
+class AvailabilityDeleteView(generics.DestroyAPIView):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+
+class ReservationListCreateView(generics.ListCreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+class ReservationDeleteView(generics.DestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
