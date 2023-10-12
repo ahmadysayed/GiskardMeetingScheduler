d3cf0d21641a2f766bcc54999b70db3f2b830194
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-12 13:17:28 +0200

    Model

--- meeting_scheduler/scheduler/models.py
+++ meeting_scheduler/scheduler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Availability(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+class Reservation(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    email = models.EmailField()
