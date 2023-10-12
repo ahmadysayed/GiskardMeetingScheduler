bfce78afa83e0d2e50f9f9df9ae35d604490b190
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-12 13:19:36 +0200

    Serializer

---
+++ meeting_scheduler/scheduler/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Availability, Reservation
+
+class AvailabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Availability
+        fields = '__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
