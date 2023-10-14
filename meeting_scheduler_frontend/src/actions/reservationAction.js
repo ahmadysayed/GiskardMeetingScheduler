408f2ceef21d62774f6873b1878dc971512d7e98
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-14 17:38:25 +0200

    Reservation Actions

--- meeting_scheduler_frontend/src/actions/reservationAction.js
+++ meeting_scheduler_frontend/src/actions/reservationAction.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { 
+    RESERVATION_LIST_REQUEST,
+    RESERVATION_LIST_SUCCESS,
+    RESERVATION_LIST_FAIL,
+
     RESERVATION_CREATE_REQUEST, 
     RESERVATION_CREATE_SUCCESS, 
     RESERVATION_CREATE_FAIL,
@@ -8,6 +12,41 @@ import {
     RESERVATION_DELETE_SUCCESS,
     RESERVATION_DELETE_FAIL
 } from "../constants/reservationConstants";
+
+
+
+
+export const listReservation = () => async (dispatch, getState) => {
+    try {
+      dispatch({ type: RESERVATION_LIST_REQUEST });
+
+      const {
+        userLogin: { userInfo },
+      } = getState()
+
+    const config = {
+        headers: {
+            'Content-type': 'application/json',
+            Authorization: `Bearer ${userInfo.token}`
+        }
+    }
+  
+      const { data } = await axios.get('http://localhost:8000/api/reservations/', config);
+  
+      dispatch({
+        type: RESERVATION_LIST_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: RESERVATION_LIST_FAIL,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+      });
+    }
+  };
 
 export const createReservation = (reservationData) => async (dispatch, getState) => {
     try {
