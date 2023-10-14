cb433fd2fffc4f9398654e0016e9328a4f283cd7
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-14 17:40:29 +0200

    Endpoint URLS

--- meeting_scheduler/scheduler/urls.py
+++ meeting_scheduler/scheduler/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('availabilities/', list_availabilities, name='list_availabilities'),
+    path('reservations/', list_reservation, name='list_reservations'),
     path('availabilities/create/', create_availability, name='create_availability'),
     path('availabilities/delete/<int:availability_id>/', delete_availability, name='delete_availability'),
     path('reservations/create/', create_reservation, name='create_reservation'),
